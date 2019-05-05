47ff05ad67abc5d2e9a9250c2f9c94a82d8e5ddd
Maneesha C D <maneeshacd@gmail.com>
2019-05-05 16:14:09 +0530

    Thermostat seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,40 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Thermostat.create(
+  household_token: 'pypNs7Zxf1onDDDzVmQf',
+  address: 'Europa-Allee 50; 60327 Frankfurt am Main, Germany'
+)
+Thermostat.create(
+  household_token: 'Lw4Pz7DMPQI5i5JT873y',
+  address: 'Hohenzollernring 56, 48145 Münster, Germany'
+)
+Thermostat.create(
+  household_token: 'rpvSf9FAaS0cad1PD4M7',
+  address: 'Vogelsanger Weg 80; 40470 Düsseldorf, Germany'
+)
+Thermostat.create(
+  household_token: 'FvJvgs7v8COV4sxxzJaw',
+  address: '50; 14057 Berlin, Germany'
+)
+Thermostat.create(
+  household_token: 'txAjzjyanWwNqRaiQaDt',
+  address: 'Bonner Talweg 100; D-53113 Bonn'
+)
+Thermostat.create(
+  household_token: 'XQ5phSVtPkY7ZFz1FX46',
+  address: 'Nauheimer Str. 100; 70372 Stuttgart, Germany'
+)
+Thermostat.create(
+  household_token: 'g34NKJW9gdhSQkWuZZ7j',
+  address: '7-1 Kozan-cho, Yamaguchi city'
+)
+Thermostat.create(
+  household_token: 'jR1NnSq1vg2bovXcQBoP',
+  address: '119 Odono-oji, Yamaguchi city'
+)
+Thermostat.create(
+  household_token: 'MQBZIiUOF7KwOOsAzZIu',
+  address: '4-1B Kameyama-cho, Yamaguchi city'
+)
+Thermostat.create(
+  household_token: 'oYsTrr8OzOKLMkeLu9HF',
+  address: '1-2-7 Tenge Yamaguchi city'
+)
